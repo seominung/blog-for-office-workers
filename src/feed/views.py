5c0e40fe3bb524ca87ea96470e1248ecd0cc66aa
seominung <seominung@gmail.com>
2019-05-23 00:43:41 +0900

    3-07

--- src/feed/views.py
+++ src/feed/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Article, Comment, HashTag
 
 # Create your views here.
 def index(request):
-    pass
+    article_list = Article.objects.all()
+    hashtag_list = HashTag.objects.all()
+    ctx = {
+        "article_list" : article_list,
+        "hashtag_list" : hashtag_list
+    }
+    return render(request, "index.html", ctx)
 
 def detail(request):
     pass
